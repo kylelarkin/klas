901ed99fccf4b6f3094f33248a45ccc0b24201cb
Alex Stockwell <astockwell@psstudios.com>
2012-10-24 15:56:03 -0700

    Fixed page structure

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?><!-- start loop -->
 
 		<section>
 		
-			<?php if(have_posts()) : while(have_posts()) : the_post(); ?><!-- start loop -->
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?><!-- end loop -->
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
 			
 		</section>
 		
 		<?php get_sidebar(); ?>
 
+	<?php endwhile; endif; ?><!-- end loop -->
 <?php get_footer(); ?>
