73ca58915032729c17668c868053bc61c0971ae3
Alex Stockwell <astockwell@psstudios.com>
2013-01-31 16:51:51 -0700

    Added static site files

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" class="searchform" action="<?php bloginfo('url'); ?>/">
-	<input type="text" placeholder="Search" name="s" id="s" />
-	<input type="submit" id="searchsubmit" value="Search" />
-</form>
