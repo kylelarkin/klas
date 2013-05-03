7126742d5587a937af67f72819aae081435f6269
micalexander <micalexander@hotmail.com>
2013-05-03 10:09:08 -0700

    changed letter casing

--- klas-specific-plugin/klas-plugin.php
+++ klas-specific-plugin/klas-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name:  Klas Site Specific Plugin
+Plugin Name:  klas Site Specific Plugin
 Description: Site specific code changes for klas
 */
 /* Start Adding Functions Below this Line */
