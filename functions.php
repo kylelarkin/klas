42017258b498e7ffa1c21b9c7cef67f4b8b1a100
micalexander <micalexander@hotmail.com>
2013-05-03 10:46:36 -0700

    now the site specific plugin auto activates on install

--- functions.php
+++ functions.php
@@ -101,4 +101,21 @@ if ( ! function_exists( '_s_comment' ) ) :
 	}
 endif; // ends check for _s_comment()
 
+function run_activate_plugin( $plugin ) {
+    $current = get_option( 'active_plugins' );
+    $plugin = plugin_basename( trim( $plugin ) );
+
+    if ( !in_array( $plugin, $current ) ) {
+        $current[] = $plugin;
+        sort( $current );
+        do_action( 'activate_plugin', trim( $plugin ) );
+        update_option( 'active_plugins', $current );
+        do_action( 'activate_' . trim( $plugin ) );
+        do_action( 'activated_plugin', trim( $plugin) );
+    }
+
+    return null;
+}
+run_activate_plugin( 'klas-specific-plugin/klas-plugin.php' );
+
 ?>
