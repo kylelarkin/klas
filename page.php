dff52378ba09460d4098165dd3ad66c5d8890bcb
Alex Stockwell <astockwell@gmail.com>
2013-07-31 17:31:27 -0700

    Indention fixes, page.php

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 	<?php if(have_posts()) : while(have_posts()) : the_post(); ?><!-- start loop -->
 
-		<section>
+			<section>
 		
-			<h1><?php the_title(); ?></h1>
-			<?php the_content(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
 			
-		</section>
+			</section>
 		
-		<?php get_sidebar(); ?>
+			<?php get_sidebar(); ?>
 
 	<?php endwhile; endif; ?><!-- end loop -->
 <?php get_footer(); ?>
